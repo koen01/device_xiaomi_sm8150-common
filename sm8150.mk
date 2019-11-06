b194540c18d7ae987e55f1ecd5a4fb79d0be57d1
Akash Srivastava <akashniki@gmail.com>
2019-11-06 23:37:10 +0100

    sm8150-common: Build config files

--- sm8150.mk
+++ sm8150.mk
@@ -139,6 +139,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# For config.fs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Fstab
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
