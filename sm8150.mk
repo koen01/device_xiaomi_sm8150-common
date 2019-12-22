693ee234d9888d319d0a1a86d57865ec2119de77
Akash <akashniki@gmail.com>
2019-12-22 19:19:34 +0530

    sm8150-common: Add Alipay support

--- sm8150.mk
+++ sm8150.mk
@@ -195,6 +195,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # IPA
 PRODUCT_PACKAGES += \
     ipacm \
